4524dc881a37a51ec5342cc7ae640242d8ac14ad
Timo <487384@student.saxion.nl>
2023-11-06 21:22:24 +0100

    will continue later

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,10 +2,15 @@ extends Area2D
 
 @export var speed = 400
 var screen_size
+var sprite_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	
+	var size = $PlayerSprite.texture.get_size()
+	# Divide by 2 since position is in the middle of character
+	sprite_size = Vector2(size * $PlayerSprite.scale) / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +20,9 @@ func _process(delta):
 	velocity = input * speed
 	
 	if velocity.length() > 0:
+		# Normalize velocity so diagonal movement is the same speed as regular
 		velocity = velocity.normalized() * speed
 	
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	# Keep character within screen bounds
+	position = position.clamp(Vector2.ZERO + sprite_size, screen_size - sprite_size)
